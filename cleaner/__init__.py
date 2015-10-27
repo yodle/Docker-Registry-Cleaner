9ae692b25dfb933b92c020b127856d5133f775b4
john.downs <john.downs@yodle.com>
2015-10-27 12:16:50 -0400

    run parameter tests

--- cleaner/__init__.py
+++ cleaner/__init__.py
@@ -1,1 +1,12 @@
-stuff = 0
+untag = lambda: True
+images = lambda: True
+scrub = lambda: True
+
+commands = {'untag': untag,
+            'images': images,
+            'scrub': scrub}
+
+def run(command):
+    if not command in commands:
+        raise ValueError()
+    return commands[command]()
--- test/cli_test.py
+++ test/cli_test.py
@@ -1,3 +1,16 @@
 import cleaner
 
-foo = cleaner.stuff
+def test_parameters():
+    keys = cleaner.commands.keys()
+    for k in ['untag', 'images', 'scrub']:
+        assert k in keys
+
+def test_bad_parameter_is_error():
+    try:
+        cleaner.run('not-a-command')
+        assert False
+    except ValueError as v:
+        assert True
+        
+def test_good_parameter_is_True():
+    assert cleaner.run('untag')
