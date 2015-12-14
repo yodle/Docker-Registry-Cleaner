6965eb59b60626c2e6707bc99d205ebfc59888f4
John Downs <john.downs@acm.org>
2015-12-14 08:45:32 -0500

    making credentials more robust to use properties file

--- cleaner/credentials.py
+++ cleaner/credentials.py
@@ -1,8 +1,26 @@
 import os
 
-REGISTRY_USERNAME = "DOCKER_REGISTRY_USER"
-REGISTRY_PASSWORD = "DOCKER_REGISTRY_PASS"
+REGISTRY_USERNAME = "registry_username"
+REGISTRY_PASSWORD = "registry_password"
 
-username = os.getenv(REGISTRY_USERNAME)
-password = os.getenv(REGISTRY_PASSWORD)
+try:
+    with open("credentials.properties") as cred_file:
+        data = cred_file.readline().split("=")
+        if data[0] == REGISTRY_USERNAME
+            username = data[1]
+        elif data[0] = REGISTRY_PASSWORD
+            password = data[1]
+except FileNotFoundError:
+    pass
+
+
+try:
+    username
+except NameError:
+    username = os.getenv(REGISTRY_USERNAME)
+
+try:
+    password
+except NameError:
+    password = os.getenv(REGISTRY_PASSWORD)
 
