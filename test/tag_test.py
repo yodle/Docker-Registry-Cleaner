a9ddbbaa011c21cf99c7134bd01f47e6f9c21cf9
john.downs <john.downs@yodle.com>
2015-12-16 13:07:42 -0500

    adding tests for tag object

--- test/tag_test.py
+++ test/tag_test.py
@@ -1,3 +1,19 @@
+import tempfile
 from cleaner import tag
 
-#todo: test memoization
+t = tempfile.NamedTemporaryFile(prefix="tag_", dir="/tmp")
+under_test = tag.Tag(t.name)
+
+def test_memoization():
+    assert not hasattr(under_test, '_name')
+    expected_name = t.name.split('/')[-1][4:]
+    assert expected_name == under_test.tag
+    assert hasattr(under_test, '_name')
+
+    assert not hasattr(under_test, '_mtime')
+    under_test.mtime
+    assert hasattr(under_test, '_mtime')
+
+    assert not hasattr(under_test, '_image')
+    under_test.image
+    assert hasattr(under_test, '_image')
--- test/test_dockerproject.py
+++ test/test_dockerproject.py
@@ -1,3 +1,14 @@
 from cleaner import docker_project
 
-#todo: test memoization
+under_test = docker_project.DockerProject("some/path")
+
+docker_project.os.listdir = lambda d: ["tag_file1", "tag_file2"]
+
+def test_name_and_path_are_parsed_correctly():
+    assert under_test.path == "some/path"
+    assert under_test.name == "path"
+
+def test_tagfiles_are_memoized():
+    assert not hasattr(under_test, "_tagfiles")
+    assert under_test.tagfiles == ["tag_file1", "tag_file2"]
+    assert hasattr(under_test, "_tagfiles")
